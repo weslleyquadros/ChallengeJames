843796a3536b583c8e6dbbf06e3a554348e0e619
Weslley Quadros <weslleyquadros@brq.com>
2020-12-08 09:03:54 -0300

    Initial Commit

---
+++ ExampleMVVM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ExampleMVVM
+//
+//  Created by Weslley Quadros on 08/12/20.
+//  Copyright © 2020 Weslley Quadros. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
